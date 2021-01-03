b3fcc340de27d9b645209d3854e929c7019d2887
CarolinaChamorro <carolinalisicsb@gmail.com>
2021-01-03 18:24:42 -0500

    transiccion

--- Juego-Navidad/Assets/Scripts/PlayerJoystick.cs
+++ Juego-Navidad/Assets/Scripts/PlayerJoystick.cs
@@ -4,13 +4,56 @@ using UnityEngine;
 
 public class PlayerJoystick : MonoBehaviour
 {
-    public float speed;
+    private float horizontalMove = 0f;
+    private float verticalMove = 0f;
+    public float runSpeed=2;
+    public float runSpeedHorizontal=2;
+    public float runSpeedVertical = 2;
     public FixedJoystick fixedJoystick;
-    public Transform player;
+    Rigidbody2D rb2D;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+
+    public void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
 
     public void FixedUpdate() 
     {
-        transform.position = new Vector3 (player.position.x + fixedJoystick.Horizontal*speed, player.position.y + fixedJoystick.Vertical*speed, player.position.z);
-        //transform.rotation = new Vector3 (player.rotation.x * 0, player.rotation.y * 0, player.rotation.z * 0);
+        horizontalMove = fixedJoystick.Horizontal * runSpeedHorizontal;
+        verticalMove = fixedJoystick.Vertical * runSpeedVertical;
+        transform.position += new Vector3(horizontalMove, verticalMove, 0) * Time.deltaTime * runSpeed;
+    }
+
+    private void Update()
+    {
+        if (horizontalMove > 0)
+        {
+            spriteRenderer.flipX = false;
+            animator.SetInteger("State", 4);
+        }
+        else if (horizontalMove < 0)
+        {
+            spriteRenderer.flipX = true;
+            animator.SetInteger("State", 3);
+        }
+        else
+        {
+            animator.SetInteger("State", 0);
+        }
+
+        if (verticalMove > 0)
+        {
+            animator.SetInteger("State",1);
+        }
+        else if (verticalMove < 0)
+        {
+            animator.SetInteger("State", 2);
+        }
+        else
+        {
+            animator.SetInteger("State", 0);
+        }
     }
 }
