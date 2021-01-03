1999c1e66013f14de03b45e9ac1798fdd6d10f97
DAFAVAC <52226201+DAFAVAC@users.noreply.github.com>
2021-01-03 11:54:32 -0500

    tileset de Quito norte en dos partes

---
+++ 64x64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="64x64" tilewidth="21" tileheight="21" tilecount="9" columns="3">
+ <image source="../../Imágenes/tileset/64x64.png" width="64" height="64"/>
+</tileset>
---
+++ kissclipart-rpg-maker-mv-tower-clipart-rpg-maker-mv-tile-based-0a3ef3a184d93794-300x300.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="kissclipart-rpg-maker-mv-tower-clipart-rpg-maker-mv-tile-based-0a3ef3a184d93794-300x300" tilewidth="21" tileheight="21" margin="2" tilecount="196" columns="14">
+ <image source="../../Imágenes/tileset/kissclipart-rpg-maker-mv-tower-clipart-rpg-maker-mv-tile-based-0a3ef3a184d93794-300x300.png" width="300" height="300"/>
+</tileset>
