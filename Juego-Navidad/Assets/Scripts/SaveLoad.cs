f6945866409ab0a6d98306d4ef28d991127ab56e
elmer2210 <elmerrivadeneira@gmail.com>
2021-01-30 18:37:00 -0500

    Mapa y guardar el avance

---
+++ Juego-Navidad/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic; 
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+using UnityEngine;
+
+public static class SaveLoad {
+
+    public static List<Game> savedGames = new List<Game>();
+
+    //Guardamos la Partida
+    public static void Save(){
+        savedGames.Add(Game.current);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
+        bf.Serialize(file, SaveLoad.savedGames);
+        file.Close();
+    } 
+
+    //Cargamos datos de la artida guarda   
+    public static void load(){
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            SaveLoad.savedGames = (List<Game>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+ 
+}
