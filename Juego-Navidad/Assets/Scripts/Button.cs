d1059a75e96873a150c64e5fe30bd87f62929bce
elmer2210 <elmerrivadeneira@gmail.com>
2021-03-02 13:37:57 -0500

    sonido agregado

---
+++ Juego-Navidad/Assets/Scripts/Button.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Button : MonoBehaviour
+{
+    Player PlayerMove;
+     //Audio Atacar 
+   public AudioSource atack;
+   //public AudioClip clip;  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerMove = GameObject.FindObjectOfType<Player>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //reporducior sonidom  
+    /*public void Sound (){
+        atack.Play();
+    }*/
+
+    //Acción derecha
+    public void derechaON()
+    {
+        PlayerMove.XMOVEMENT(1);
+       
+    }
+    public void derechaOFF()
+    {
+        PlayerMove.XMOVEMENT(0);
+        
+    }
+
+    //Acción izquierda
+    public void izquierdaON()
+    {
+        PlayerMove.XMOVEMENT(-1);
+    }
+    public void izquierdaOFF()
+    {
+        PlayerMove.XMOVEMENT(0);
+    }
+
+    //Acción arriba
+    public void arribaON()
+    {
+        PlayerMove.YMOVEMENT(1);
+    }
+    public void arribaOFF()
+    {
+        PlayerMove.YMOVEMENT(0);
+    }
+
+    //Acción abajo
+    public void abajoON()
+    {
+        PlayerMove.YMOVEMENT(-1);
+    }
+    public void abajoOFF()
+    {
+        PlayerMove.YMOVEMENT(0);
+    }
+
+    public void AtacarOn()
+    {
+        float derechaX = 1;
+        float izquierdaX = -1;
+        float arribaY = 1;
+        float abajoY = -1;
+        if (derechaX == 1)
+        {
+            atack.Play();
+            PlayerMove.atomizador(derechaX);
+
+        }
+        if (izquierdaX == -1)
+        {
+            atack.Play();
+            PlayerMove.atomizador(izquierdaX);
+        }
+        if (arribaY == 1)
+        {
+            atack.Play();
+            PlayerMove.atomizador(arribaY);
+        }
+        if (abajoY == -1)
+        {
+            atack.Play();
+            PlayerMove.atomizador(abajoY);
+        }
+    }
+
+        public void AtacarOff()
+    {
+        float derechaX = 0;
+        float izquierdaX = 0;
+        float arribaY = 0;
+        float abajoY = 0;
+        if (derechaX == 0)
+        {
+            
+            PlayerMove.atomizador(derechaX);
+        }
+        if (izquierdaX == 0)
+        {
+           
+            PlayerMove.atomizador(izquierdaX);
+        }
+        if (arribaY == 0)
+        {
+            
+            PlayerMove.atomizador(arribaY);
+        }
+        if (abajoY == 0)
+        {
+            
+            PlayerMove.atomizador(abajoY);
+        }
+    }
+
+}
---
+++ Juego-Navidad/Assets/Scripts/Player.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //Script 
+    private Rigidbody2D TheBody = null;
+    private Transform TheTransform = null;
+    public float MaxSpeed = 4.0f;
+    private float Vdirection = 0.0f;
+    //private float Hdirection = 0.0f;
+    private float Xmovement = 0.0f;
+    private float Ymovement = 0.0f;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+    public float x1 = 0.0f;
+    public float y1 = 0.0f;
+
+    void Awake()
+    {
+        TheBody = GetComponent<Rigidbody2D>();
+        TheTransform = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+
+        Vector3 Direction = new Vector3(Xmovement, Ymovement, 0);
+
+        TheBody.AddForce(Direction.normalized * MaxSpeed);
+
+
+        if (Direction.y != 0 || Direction.x != 0)
+        {
+            TheBody.AddForce(Direction.normalized * MaxSpeed);
+        }
+        else
+        {
+            TheBody.velocity = new Vector3(0, 0, 0);
+        }
+
+
+        TheBody.velocity = new Vector2(
+            Mathf.Clamp(TheBody.velocity.x, -MaxSpeed, MaxSpeed),
+            Mathf.Clamp(TheBody.velocity.y, -MaxSpeed, MaxSpeed)
+            );
+
+    }
+
+    public void Vertical(float Direction)
+    {
+        Vdirection = Direction;
+        Debug.Log(Vdirection);
+    }
+
+    public void XMOVEMENT(float x)
+    {
+        Xmovement = x;
+        if (Xmovement == 1)
+        {
+
+            animator.SetBool("WalkingRight", true);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+        else if (Xmovement == -1)
+        {
+            animator.SetBool("WalkingLeft", true);
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+        else if (Xmovement == 0)
+        {
+
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+    }
+
+    public void YMOVEMENT(float y)
+    {
+        Ymovement = y;
+
+        if (Ymovement == 1)
+        {
+            animator.SetBool("WalkingBack", true);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingRight", false);
+        }
+        else if (Ymovement == -1)
+        {
+            animator.SetBool("WalkingFront", true);
+            animator.SetBool("WalkingBack", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingRight", false);
+        }
+        else if (Ymovement == 0)
+        {
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+    }
+
+    public void atomizador(float x)
+    {
+        x1 = x;
+        y1 = x;
+        if (Xmovement==1 && x1==1)
+        {
+            animator.SetBool("AttackFront", false);
+            animator.SetBool("AttackBack", false);
+            animator.SetBool("AttackLeft", false);
+            animator.SetBool("AttackRight", true);
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+        if (Xmovement== -1 && x1== -1)
+        {
+            animator.SetBool("AttackFront", false);
+            animator.SetBool("AttackBack", false);
+            animator.SetBool("AttackLeft", true);
+            animator.SetBool("AttackRight", false);
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+
+        if(Ymovement==1 && y1 == 1)
+        {
+            animator.SetBool("AttackFront", false);
+            animator.SetBool("AttackBack", true);
+            animator.SetBool("AttackLeft", false);
+            animator.SetBool("AttackRight", false);
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+        if (Ymovement == -1 && y1 == -1)
+        {
+            animator.SetBool("AttackFront", true);
+            animator.SetBool("AttackBack", false);
+            animator.SetBool("AttackLeft", false);
+            animator.SetBool("AttackRight", false);
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+
+        if (Xmovement == 0 && x1 == 0)
+        {
+            animator.SetBool("AttackFront", false);
+            animator.SetBool("AttackBack", false);
+            animator.SetBool("AttackLeft", false);
+            animator.SetBool("AttackRight", false);
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+        if (Ymovement == 0 && y1 == 0)
+        {
+            animator.SetBool("AttackFront", false);
+            animator.SetBool("AttackBack", false);
+            animator.SetBool("AttackLeft", false);
+            animator.SetBool("AttackRight", false);
+            animator.SetBool("WalkingRight", false);
+            animator.SetBool("WalkingLeft", false);
+            animator.SetBool("WalkingFront", false);
+            animator.SetBool("WalkingBack", false);
+        }
+
+    }
+    private void Update()
+    {
+
+    }
+
+}
+
+
+
+
+
+
+
