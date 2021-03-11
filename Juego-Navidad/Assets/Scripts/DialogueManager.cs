b6717807661f2f6dcf75baa597fd5f87179e4b23
CarolinaChamorro <carolinalisicsb@gmail.com>
2021-03-11 15:42:13 -0500

    Cambios finales- dialogos interactivos

---
+++ Juego-Navidad/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    Queue<string> sentences;
+
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI displayText;
+
+    string activeSentence;
+    public float typingSpeed;
+
+    //buttons
+    //public GameObject botonQuitar;
+    public GameObject botonContinuar;
+
+    //AudioSource myAudio;
+    //public AudioClip speakSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        botonContinuar.SetActive(false);
+        //botonQuitar.SetActive(false);
+        //myAudio = GetComponent<AudioSource>();
+    }
+
+    void StartDialogue()
+    {
+        
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentenceList)
+        {
+            sentences.Enqueue(sentence);
+            
+        }
+
+        DisplayNextSentence();
+    }
+
+   public void DisplayNextSentence()
+    {
+        
+        if (sentences.Count <= 0)
+        {
+            displayText.text = activeSentence;
+            
+            return;
+        }
+
+        activeSentence = sentences.Dequeue();
+        displayText.text = activeSentence;
+        botonContinuar.SetActive(true);
+        StopAllCoroutines();
+        StartCoroutine(TypeTheSentence(activeSentence));
+    }
+
+    IEnumerator TypeTheSentence(string sentence)
+    {
+        displayText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            displayText.text += letter;
+            //myAudio.PlayOneShot(speakSound);
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            dialoguePanel.SetActive(true);
+            botonContinuar.SetActive(true);
+            //botonQuitar.SetActive(false);
+            StartDialogue();
+        }
+    }
+
+
+    private void OntriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+          if (botonContinuar && displayText.text==activeSentence)
+                {
+                    DisplayNextSentence();
+                }
+            
+        }
+    }
+
+    public void botonCerrar()
+    {
+        dialoguePanel.SetActive(false);
+        botonContinuar.SetActive(false);
+    }
+    private void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Player"))
+        {
+            dialoguePanel.SetActive(false);
+            botonContinuar.SetActive(true);
+            //botonQuitar.SetActive(false);
+        }
+    }
+}
--- Juego-Navidad/Assets/Tiled2Unity/Scripts/Editor/TiledAssetPostProcessor.cs
+++ Juego-Navidad/Assets/Tiled2Unity/Scripts/Editor/TiledAssetPostProcessor.cs
@@ -135,7 +135,7 @@ namespace Tiled2Unity
 
             // We will create and assign our own materials.
             // This gives us more control over their construction.
-            modelImporter.importMaterials = false;
+            //modelImporter.importMaterials = false;
 
 #if UNITY_5_6_OR_NEWER
             modelImporter.keepQuads = true;
