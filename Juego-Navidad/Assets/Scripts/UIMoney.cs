688d749e7cb7cda668e75e16f5d04d4eabb89817
DAFAVAC <dfc.vacas@yavirac.edu.ec>
2021-03-11 14:02:48 -0500

    game completed map and money/enemy

---
+++ Juego-Navidad/Assets/Scripts/UIMoney.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIMoney : MonoBehaviour
+{
+    public Text score;
+    public int Cantidad = 5;
+
+    void update() 
+    {
+        score.text = Cantidad.ToString();
+    
+    }
+}
